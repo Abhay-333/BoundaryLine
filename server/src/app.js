7cf776670fb59346fff97e51a42be1d4f9cadd0b
avyaanverma <avyaanver@gmail.com>
2026-06-13 20:44:35 +0530

    middleware added limit requests 10mb

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,10 @@ export default function createApp(){
     if(env.NODE_ENV === "production"){
         app.use(morgan("dev"));
     }
+    
+    // parsing req.body
+    app.use(express.json({limit: "3mb"})); // limiting req.body to max 10mb
+    app.use(express.urlencoded({extended: true, limit: "3mb"})) // limitng html form data to max 10mb
 
     app.get("/health", (req,res)=>{
         res.json({
--- server/src/config/env.js
+++ server/src/config/env.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import {z} from "zod";
 import appConstant from "../constant/app.constant.js";
 dotenv.config();
-
 // creating zod object to validate env Schema
 const envSchema = z.object({
     PORT: z.coerce.number().default(appConstant.PORT),
@@ -16,6 +15,6 @@ const parsed = envSchema.safeParse(process.env);
 
 
 if(!parsed.success){
-    logger.error(parsed.error.format());
+    console.error(parsed.error.format());
 }
 export default parsed.data;
