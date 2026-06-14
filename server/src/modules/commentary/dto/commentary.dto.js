670e574a0d4f194be0cb2b4a0634d5e6b44352fd
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-14 20:07:38 +0530

    made the commentary data mahesh object more accurate

--- server/src/modules/commentary/dto/commentary.dto.js
+++ server/src/modules/commentary/dto/commentary.dto.js
@@ -1,5 +1,29 @@
-export {
-  createCommentarySchema,
-  commentaryParamSchema,
-  getCommentaryByMatchSchema
-} from "../validators/commentary.validator.js";
+/**
+ * Commentary DTO
+ *
+ * Database ka raw object
+ * clean format me convert karega
+ */
+export default class CommentaryDTO {
+  constructor(commentary) {
+    // Mongo _id ko id bana diya
+    this.id = commentary._id;
+
+    // Required fields
+    this.matchId = commentary.matchId;
+
+    this.over = commentary.over;
+
+    this.ball = commentary.ball;
+
+    this.text = commentary.text;
+
+    this.type = commentary.type;
+
+    // User info
+    this.createdBy = commentary.createdBy;
+
+    // Dates
+    this.createdAt = commentary.createdAt;
+  }
+}
