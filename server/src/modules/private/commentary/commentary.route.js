8caddeed688b768deb597560294839d75f446b38
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-15 23:06:47 +0530

    added the middleware of authroisation and validators of the commentary

--- server/src/modules/private/commentary/commentary.route.js
+++ server/src/modules/private/commentary/commentary.route.js
@@ -1,28 +1,58 @@
 import CommentaryController from "./commentary.controller.js";
 import express from "express";
 
+import validateRequest from "../middleware/validateRequest.js";
+
+import {
+  authenticate,
+  authorize,
+} from "../middleware/auth.middleware.js";
+
+import {
+  createCommentarySchema,
+  commentaryParamSchema,
+  getCommentaryByMatchSchema,
+} from "../validators/commentary.validator.js";
+
 const router = express.Router();
 
 const commentaryController = new CommentaryController();
 
-// Controller object
-
 /**
  * POST /api/commentary
  * New commentary create karega
+ * Only ADMIN, SUPER_ADMIN, SCORER
  */
-router.post("/", commentaryController.addCommentary);
+router.post(
+  "/",
+  authenticate,
+  authorize("SUPER_ADMIN", "ADMIN", "SCORER"),
+  validateRequest(createCommentarySchema),
+  commentaryController.addCommentary,
+);
 
 /**
  * DELETE /api/commentary/:id
  * Commentary delete karega
+ * Only ADMIN & SUPER_ADMIN
  */
-router.delete("/:id", commentaryController.deleteCommentary);
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("SUPER_ADMIN", "ADMIN"),
+  validateRequest(commentaryParamSchema),
+  commentaryController.deleteCommentary,
+);
 
 /**
  * GET /api/commentary/match/:matchId
- * Match ki commentary fetch karega
+ * Match ki commentary fetch karega Logged-in users only
  */
-router.get("/match/:matchId", commentaryController.getCommentaryByMatch);
+router.get(
+  "/match/:matchId",
+  authenticate,
+  validateRequest(getCommentaryByMatchSchema),
+  commentaryController.getCommentaryByMatch,
+);
 
 export default router;
